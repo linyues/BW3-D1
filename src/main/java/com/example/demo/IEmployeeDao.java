fed0138db87625c3b3773df49dcc794d532c01ee
linyues <2694166721@qq.com>
2021-10-08 16:44:39 +0800

    IEmployeeDao

---
+++ src/main/java/com/example/demo/IEmployeeDao.java
@@ -0,0 +1,9 @@
+package src.main.java.com.example.demo;
+import java.util.List;
+public interface IEmployeeDao {
+    Employee getEmployeeByName(Integer id);
+    List<Employee> getEmployeeList();
+    String addEmployee(Employee employee);
+    String updateEmployee(Integer id,Employee employee);
+    String deleteEmployee(Integer id);
+}
